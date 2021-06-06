9a41bff9dc4ab568ddee3a5171e78a5cd0816ca0
kai <2857693944@qq.com>
2021-06-06 12:53:12 +0800

    test touch driver

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
+
 #include "main.h"
 #include "i2c.h"
 #include "usart.h"
@@ -30,6 +31,9 @@
 #include "touch.h"
 #include "lvgl.h"
 #include "lvgl/porting/lv_port_disp.h"
+#include "lvgl/porting/lv_port_indev.h"
+#include <gui_guider.h>
+#include <events_init.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -49,71 +53,13 @@ extern const uint8_t touxiang_map[];
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-LV_IMG_DECLARE(watch_bg);
-LV_IMG_DECLARE(hour);
-LV_IMG_DECLARE(minute);
-LV_IMG_DECLARE(second);
-
-lv_obj_t *lvMinute;
-lv_obj_t *lvHour;
-lv_obj_t *lvSecond;
-
-uint8_t Minute = 59;
-uint8_t Hour = 8;
-uint8_t Second = 2;
+lv_ui guider_ui;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-static void update_time(struct _lv_task_t *arg) {
-    if (lvHour != NULL) {
-        // Hour,Minute,Second;
-        uint16_t h = Hour * 300 + Minute / 12 % 12 * 60;
-        lv_img_set_angle(lvHour, h);
-        lv_img_set_angle(lvMinute, Minute * 6 * 10);
-        lv_img_set_angle(lvSecond, Second * 6 * 10);
-    }
-    if (++Second >= 60) {
-        Second = 0;
-        if (++Minute >= 60) {
-            Minute = 0;
-            if (++Hour >= 12) Hour = 0;
-        }
-    }
-}
 
-void analog(lv_obj_t *win) {
-    lv_obj_t *central = win;
-
-    lv_obj_set_size(central, LV_HOR_RES_MAX, LV_VER_RES_MAX);
-    lv_obj_set_pos(central, 0, 0);
-
-    lv_obj_t *img = lv_img_create(central, NULL);
-    lv_img_set_src(img, &watch_bg);
-
-    lv_obj_set_size(img, 200, 200);
-    lv_obj_set_auto_realign(img, true);
-    lv_obj_align(img, central, LV_ALIGN_CENTER, 0, 0);
-
-    lvHour = lv_img_create(central, NULL);
-    lv_img_set_src(lvHour, &hour);
-    lv_obj_align(lvHour, img, LV_ALIGN_CENTER, 0, 0);
-    uint16_t h = Hour * 300 + Minute / 12 % 12 * 60;
-    lv_img_set_angle(lvHour, h);
-
-    lvMinute = lv_img_create(central, NULL);
-    lv_img_set_src(lvMinute, &minute);
-    lv_obj_align(lvMinute, img, LV_ALIGN_CENTER, 0, 0);
-    lv_img_set_angle(lvHour, Minute * 60);
-
-    lvSecond = lv_img_create(central, NULL);
-    lv_img_set_src(lvSecond, &second);
-    lv_obj_align(lvSecond, img, LV_ALIGN_CENTER, 0, 0);
-    lv_img_set_angle(lvSecond, Second * 60);
-
-    lv_task_create(update_time, 1000, LV_TASK_PRIO_LOW, NULL);
-}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -159,7 +105,9 @@ int main(void) {
 //    uint16_t y;
     lv_init();
     lv_port_disp_init();
-    analog(lv_scr_act());
+    lv_port_indev_init();
+    setup_ui(&guider_ui);
+    events_init(&guider_ui);
     /* USER CODE END 2 */
 
     /* Infinite loop */
